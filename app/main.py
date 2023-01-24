1414eede55b7501f72445622b0c82ac7954225a5
ChanthruV <chanthru2211@gmail.com>
2023-01-24 11:12:55 +1100

    update

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from starlette.responses import JSONResponse
 from joblib import load
 import pandas as pd
+import torch
+import torchvision
 #from pydantic import BaseModel
 
 app = FastAPI()
